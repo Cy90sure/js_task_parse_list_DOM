1d688ae684083e43293384a752aaa40a79eb6b7a
Maria <maria.pozniak2013@gmail.com>
2024-08-06 11:35:33 +0300

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,25 @@
+"use strict";
+const employeesElements = document.querySelectorAll("li");
+const employeesObj = [];
+employeesElements.forEach((element)=>{
+    const obj = {
+        name: element.textContent.trim(),
+        position: element.getAttribute("data-position"),
+        salary: parseFloat(element.getAttribute("data-salary").replace(/[$,]/g, "")),
+        age: element.getAttribute("data-age")
+    };
+    employeesObj.push(obj);
+});
+const employeesSorted = employeesObj.sort((a, b)=>b.salary - a.salary);
+const ul = document.querySelector("ul");
+ul.innerHTML = "";
+employeesSorted.forEach((employee)=>{
+    const li = document.createElement("li");
+    li.textContent = employee.name;
+    li.setAttribute("data-position", employee.position);
+    li.setAttribute("data-salary", "$" + employee.salary.toLocaleString("en-US"));
+    li.setAttribute("data-age", employee.age);
+    ul.appendChild(li);
+});
+
+//# sourceMappingURL=index.f75de5e1.js.map
